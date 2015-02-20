4fd66cc7a777efa96a0af71f775bf2d03867c389
AaronKolenda <kolenda.a@gmail.com>
2015-02-20 14:56:22 -0500

    getting repo data

--- index.js
+++ index.js
@@ -11,20 +11,39 @@ var getUserData = function() {
       console.log(userData);
 
       var htmlString = templates.headerUserInfo(userData);
-
       $("#head-user-info").append(htmlString);  
 
       var loginUserString = templates.loginNameInfo(userData);
-
       $("#namesavatar").append(loginUserString);  
 
       var locationJoinString = templates.locationJoinInfo(userData);
-
       $("#locationjoin").append(locationJoinString); 
 
       var followingString = templates.followingInfo(userData);
-
       $("#following").append(followingString);  
+
+
+    }
+
+  });
+
+}
+
+
+var getUserRepos = function() {
+
+  $.ajax({
+
+    url: "https://api.github.com/users/AaronKolenda/repos?access_token=85125e5a99be21e5bf22ba26ebd5faf7a388dcb4",
+
+    method: "GET",
+
+    success: function(userData) {
+
+      console.log(userData);
+
+      var repositoriesString = templates.repositoriesInfo(userData);
+      $("#repositories").append(repositoriesString); 
 
 
     }
@@ -38,27 +57,25 @@ var templates = {};
 var getTemplates = function(){
 
   var headerUserTemplateString = $("#header-user-template").text()
-
   templates.headerUserInfo = Handlebars.compile(headerUserTemplateString);
 
   var loginNameTemplateString = $("#login-name-template").text()
-
   templates.loginNameInfo = Handlebars.compile(loginNameTemplateString);
 
   var locationJoinTemplateString = $("#location-join-template").text()
-
   templates.locationJoinInfo = Handlebars.compile(locationJoinTemplateString);
 
   var followingTemplateString = $("#following-template").text()
-
   templates.followingInfo = Handlebars.compile(followingTemplateString);
 
-
+  var repositoriesTemplateString = $("#repositories-template").text()
+  templates.repositoriesInfo = Handlebars.compile(repositoriesTemplateString);
 }
 
 $(document).ready(function(){
     getTemplates();
     getUserData();
+    getUserRepos();
 
 });
 
